e4653360ac3e1269452666b7957752c839a1d996
iZeaoGamer <jameshayward14@icloud.com>
2018-12-15 15:00:13 +0000

    Fix.

--- src/SizePLayer/SizePLayerCommand.php
+++ src/SizePLayer/SizePLayerCommand.php
@@ -29,31 +29,31 @@ class SizePLayerCommand extends Command {
             if(isset($args[0])) {
                 if(is_numeric($args[0])) {
                     if($args[0] > 15) {
-                      $player->sendMessage(TF::RED. "This size must not bigger than §e15");
+                      $player->sendMessage(TF::RED. "This size cannot be bigger than §2$args[0]");
                       return true;
                     }elseif($args[0] <= 0) {
-                      $player->sendMessage(TF::RED. "This size cannot be smaller than or equal to §e0");
+                      $player->sendMessage(TF::RED. "This size cannot be smaller than or equal to §2$args[0]");
                       return true;
                     }
                     $this->plugin->size[$player->getName()] = $args[0];
                     $player->setScale((float)$args[0]);
-                    $player->sendMessage("§8§l(§a!§8)§r §aYou have changed your size to ".TF::GOLD . $args[0]."§a!");
+                    $player->sendMessage("§dYou have changed your size to §5".TF::GOLD . $args[0]."§d!");
                 }elseif($args[0] == "reset") {
                     if(!empty($this->plugin->size[$player->getName()])) {
                         unset($this->plugin->size[$player->getName()]);
                         $player->setScale(1);
-                        $player->sendMessage("§8§l(§a!§8)§r §aYou size has back to normal!");
+                        $player->sendMessage("§dYou size has back to normal!");
                     }else{
-                        $player->sendMessage("§8§l(§a!§8)§r §aYou have reseted your size!");
+                        $player->sendMessage("§dYou have reseted your size!");
                     }
                 }else{
-                    $player->sendMessage("§8» §eCommands Lists! Sizeplayer \n§8» §c/size help §7- if you don`t know the commands!\n§8» §c/size reset §7- This command reset your sizes!\n§8» §c/size (size:number) §7- This command makes you any size!");
+                    $player->sendMessage("§aCommands§5 Lists! §3Size§cPlayer\n§7/size help - §bif you don`t know the commands!\n§8» §7/size reset §7- §bThis command reset your sizes!\n§7/size (size:number) §7- §bThis command makes you any size!");
                 }
             } else {
-              $player->sendMessage(TF::RED. "The size is not a valid number!");
+              $player->sendMessage(TF::RED. "§cThe size is not a valid number!");
             }
             return true;
         }
-        $player->sendMessage(TF::RED. "You do not have permission allowed to use size command!");
+        $player->sendMessage(TF::RED. "§cThis command is only meant to be used for voters and ranked players!");
     }
 }
